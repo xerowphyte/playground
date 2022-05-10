1a75cfa13f0b41fbd369f462a49a99e4cdd4258f
xerowphyte <noahowootori2014@gmail.com>
2022-05-10 13:09:17 +0100

    newer lines of code(freecodecamp)

--- python/time_calculator.py
+++ python/time_calculator.py
@@ -2,6 +2,9 @@ from audioop import add
 
 
 def add_time(start, duration):
+    days_of_the_week_index = {"monday": 0, "tuesday": 1, "wednesday": 2, "thursday": 3, "friday": 4, "saturday": 5, "sunday": 6}
+    days_of_the_week_array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
     
     duration_tuple = duration.partition(":")
     print(duration_tuple)
@@ -19,10 +22,17 @@ def add_time(start, duration):
     if end_minutes >= 60:
         start_hours += 1
         print(start_hours)
-        end_minutes =end_minutes % 60
-    end_hours = start_hours + duration_hours % 12
+        end_minutes = end_minutes % 60
+
+    
+    amount_of_am_pm_flips = int((start_hours + duration_hours) / 12) 
+    end_hours = int((start_hours + duration_hours) % 12)
     end_minutes = end_minutes if end_minutes > 9 else "0" + str(end_minutes)
     end_hours = end_hours = 12 if end_hours == 0 else end_hours
+    #if(am_or_pm == "PM" and start_hours + duration_hours % 12) >= 12 :
+        amount_of_days += 1
+    am_or_pm = am_and_pm_flip[am_or_pm] if amount_of_am_pm_flips % 2 == 1 else am_or_pm
+    retunTime = str(end_hours) + ":" + str(end_minutes) + " " + am_or_pm
 
 
 
